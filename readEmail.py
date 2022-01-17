6c7708d0d4a68e21c73c6593df1ea91c467696d3
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2022-01-17 22:30:44 +0530

    Add files via upload

---
+++ readEmail.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import imaplib
+import email
+import schedule, time
+from crontab import CronTab
+
+
+
+
+def read_mails():
+    st.title("Email Reader")
+    host='imap.gmail.com'
+    username='rajinder@swissbeauty.in'
+    password='9711609399A'
+
+    mail=imaplib.IMAP4_SSL(host)
+    mail.login(username,password)
+    mail.select('INBOX')
+
+    _,search_data=mail.search(None,'(FROM "supply@meesho.com")','(FROM "supply@meesho.com")',"UNSEEN")
+
+
+    for i in search_data[0].split():
+        _,data=mail.fetch(i,'(RFC822)')
+        email_body=data[0][1]
+    #     # print(email_body)
+        email_body=email.message_from_bytes(email_body)
+        # if email_body['From']=='supply@meesho.com':
+        #     print('From:',email_body['From'])
+        #     print('Subject:',email_body['Subject'])
+        #     print('Body:',email_body.get_payload())
+            # mail.store(i,'+FLAGS','\Seen')
+
+
+
+# schedule.every(1).minutes.do(read_mails)
+
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
+
+cron=CronTab(user='rajinder')
+job=cron.new(command='python3 readEmail.py')
+job.minute.every(1)
+
+cron.write()
